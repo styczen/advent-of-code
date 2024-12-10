e001e26e3b018348dd29bd90bf5c9d35afd640d0
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2024-12-10 16:30:38 +0100

    WIP

--- 2024/src/bin/03.rs
+++ 2024/src/bin/03.rs
@@ -2,8 +2,8 @@ fn calculate_mul_sums(content: &str) -> u64 {
     let mut sum = 0u64;
     for m in content.split("mul(") {
         for n in m.split(")") {
-            if let Some(p) = n.split_once(',') {
-                sum += match (p.0.parse::<u64>(), p.1.parse::<u64>()) {
+            if let Some((l_str, r_str)) = n.split_once(',') {
+                sum += match (l_str.parse::<u64>(), r_str.parse::<u64>()) {
                     (Ok(l), Ok(r)) => l * r,
                     _ => 0,
                 };
@@ -16,7 +16,8 @@ fn calculate_mul_sums(content: &str) -> u64 {
 fn main() {
     println!("Day 3");
     println!("Part 1");
-    let content = aoc_2024::load_file("inputs/03");
+    let content = aoc_2024::load_file("inputs/03_test");
+    println!("{}", content);
     let sum = calculate_mul_sums(&content);
     println!("{}", sum);
     println!("Part 2");
@@ -24,10 +25,14 @@ fn main() {
     // and calculate sums for those substrings. Handle first split 
     // between start of the string and "do" or "dont" because all
     // muls there are enabled.
+
+    let does: Vec<_> = content.split("do()").collect();
+    let first = does.first();
+
     for m in content.split("do()") {
-        println!("{}", m);
-        for n in content.split("don't()") {
-            println!("{}", n);
+        println!("DO: {}", m);
+        for n in m.split("don't()") {
+            println!("DONT: {}", n);
         }
     }
 }
