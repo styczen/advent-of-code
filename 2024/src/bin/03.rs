8a9a5292690095e2ca0d16547d1a46c32ddb9a09
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2024-12-22 19:38:30 +0100

    Day 3 part 1 work but damn if you do not want to use regex

--- 2024/src/bin/03.rs
+++ 2024/src/bin/03.rs
@@ -13,26 +13,24 @@ fn calculate_mul_sums(content: &str) -> u64 {
     sum
 }
 
+fn calculate_enabled_mul_sums(content: &str) -> u64 {
+    let mut sum = 0u64;
+
+    let mut enabled = true;
+
+    // TODO
+
+    sum
+}
+
 fn main() {
     println!("Day 3");
     println!("Part 1");
-    let content = aoc_2024::load_file("inputs/03_test");
-    println!("{}", content);
+    let content = aoc_2024::load_file("inputs/03");
     let sum = calculate_mul_sums(&content);
     println!("{}", sum);
+
     println!("Part 2");
-    // TODO: Extract substrings between "do" and "dont" (in that order) 
-    // and calculate sums for those substrings. Handle first split 
-    // between start of the string and "do" or "dont" because all
-    // muls there are enabled.
-
-    let does: Vec<_> = content.split("do()").collect();
-    let first = does.first();
-
-    for m in content.split("do()") {
-        println!("DO: {}", m);
-        for n in m.split("don't()") {
-            println!("DONT: {}", n);
-        }
-    }
+    let enabled_sum = calculate_enabled_mul_sums(&content);
+    println!("{}", enabled_sum);
 }
